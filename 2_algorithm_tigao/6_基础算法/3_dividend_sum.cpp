18644863ff482792c1d5855c6e7b9f204c3ce1b2
matrixlin <matrixlin@tencent.com>
2022-08-18 22:09:53 +0800

    add dividend sum

---
+++ 2_algorithm_tigao/6_基础算法/3_dividend_sum.cpp
@@ -0,0 +1,42 @@
+// 约数之和
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int mod = 9901;
+int qmi(int a, int k) {
+    int res = 1;
+    a %= mod;
+    while(k) {
+        if (k & 1) res = res * a % mod;
+        a = a * a % mod;
+        k >>= 1;
+    }
+    return res;
+}
+
+int sum(int p, int k) {
+    if (k == 1) return 1;
+    if (k % 2== 0) return (1 + qmi(p, k/ 2)) * sum(p, k / 2) % mod;
+    return (sum(p, k - 1) + qmi(p, k - 1)) % mod;
+}
+
+int main() {
+    int a ,b; 
+    cin >> a >> b;
+    int res = 1;
+    for (int i = 2; i * i <= a; i++) 
+        if (a % i == 0) {
+            int s = 0;
+            while(a % i == 0) {
+                a /= i, s ++;
+            }
+            res = res * sum(i, b * s + 1) % mod;
+        }
+    if (a > 1) res = res * sum(a, b + 1) % mod;
+    if (a == 0) res = 0;
+    cout << res << endl;
+    return 0;
+}
