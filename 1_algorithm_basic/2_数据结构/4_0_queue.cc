b263e25ad1211c1f70ad83e0651983055c3767e9
MatrixWeb <matrixlin@tencent.com>
2024-03-09 21:41:01 +0800

    add stack queue

---
+++ 1_algorithm_basic/2_数据结构/4_0_queue.cc
@@ -0,0 +1,25 @@
+// 数组模拟队列
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e5 + 10;
+
+int m;
+int q[N], hh, tt = -1;
+
+int main() {
+    cin >> m;
+    while(m --) {
+        string op;
+        int x;
+        cin >> op;
+        if (op == "push") {
+            cin >> x;
+            q[++ tt] = x;
+        } else if (op == "pop") hh++;
+        else if (op == "empty") cout << (hh <= tt ? "NO" : "YES") << endl;
+        else cout << q[hh] << endl;
+    }
+    return 0;
+}
