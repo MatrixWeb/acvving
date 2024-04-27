be3cfa5c8456351fc2697810dce36e6861d3c061
MatrixWeb <matrixlin@tencent.com>
2024-04-27 16:39:30 +0800

    feat: add bellmanford

---
+++ 1_algorithm_basic/3_搜索与图论/5_bellmanford.cc
@@ -0,0 +1,43 @@
+// https://www.acwing.com/problem/content/855/
+// 有边数限制，求最短路
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 510, M = 10010;
+
+struct Edge {
+    int a, b, c;
+}edges[M];
+
+int n, m, k;
+int dist[N];
+int last[N];
+
+void bellman_ford() {
+    memset(dist, 0x3f, sizeof dist);
+    dist[1] = 0;
+    
+    for (int i = 0; i < k; i++) {
+        memcpy(last, dist, sizeof dist);
+        for (int j = 0; j < m; j++) {
+            auto e = edges[j];
+            dist[e.b] = min(dist[e.b], last[e.a] + e.c);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m >> k;
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edges[i] = {a, b, c};
+    }
+    bellman_ford();
+    // 无穷大可能会被更新
+    if (dist[n] > 0x3f3f3f3f/2) puts("impossible");
+    else cout << dist[n] << endl;
+    
+    return 0;
+}
