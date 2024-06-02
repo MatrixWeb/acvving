db0ffa0c958eb252941b2d45bcc7e28bc651fdb8
MatrixWeb <matrixlin@tencent.com>
2024-06-02 11:27:32 +0800

    feat: add divisor

---
+++ 1_algorithm_basic/4_数学基础/4_1_divisor.cc
@@ -0,0 +1,29 @@
+// https://www.acwing.com/problem/content/871/
+// 试除法求约数个数
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> get_divisors(int x) {
+    vector<int> res;
+    for (int i = 1; i <= x/i; i++) {
+        if (x % i == 0) {
+            res.push_back(i);
+            if (i != x/i) res.push_back(x/i);
+        }
+    }
+    sort(res.begin(), res.end());
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    while(n --) {
+        int x;
+        cin >> x;
+        auto res = get_divisors(x);
+        for (auto x : res) cout << x << ' ';
+        cout << endl;
+    }
+    return 0;
+}
---
+++ 1_algorithm_basic/4_数学基础/4_4_gcd.cc
@@ -0,0 +1,21 @@
+// https://www.acwing.com/problem/content/874/
+// 辗转相除法求最大公约数
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    return b ? gcd(b, a % b) : a;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    while(n --) {
+        int a, b;
+        cin >> a >> b;
+        
+        cout << gcd(a, b) << endl;
+    }
+    return 0;
+}
